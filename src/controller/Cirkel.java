bfc7fbb7a471c7dca554ee5361e5973c0bad18ad
REDijkstra <65535569+RutgerDijkstra@users.noreply.github.com>
2021-11-09 19:50:56 +0100

    Oefening 1.1 een klasse maken

---
+++ src/controller/Cirkel.java
@@ -0,0 +1,21 @@
+package controller;
+
+/**
+ * @author Rutger Dijkstra <r.e.dijkstra@st.hanze.nl>
+ * Deze klasse omschrijft een cirkel
+ */
+public class Cirkel {
+    public double straal;
+    public double middelpuntX;
+    public double middelpuntY;
+    public String kleur;
+
+    public double geefOmtrek(){
+        return 2 * Math.PI * straal;
+    }
+
+    public double geefOppervlakete(){
+        return Math.PI * straal * straal;
+    }
+
+}
