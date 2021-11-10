edfa8aa574588e7d47f8f5482d4fa14cae46c5fd
REDijkstra <65535569+RutgerDijkstra@users.noreply.github.com>
2021-11-10 20:20:05 +0100

    Oefening 5.3 Wrapper classes

--- src/controller/MeetkundeLauncher.java
+++ src/controller/MeetkundeLauncher.java
@@ -40,6 +40,19 @@ public class MeetkundeLauncher {
             System.out.println();
         }
 
+        int integer1 = 3;
+        Integer integer2 = Integer.valueOf(3);
+
+        Integer integer3 = 3;
+        int integer4 = Integer.valueOf(3);
+
+        System.out.println(integer1 + "\n"
+                + integer2.toString() + "\n"
+                + integer3 + "\n"
+                + Integer.valueOf(integer4).toString());
+
+
+
     }
 
     public static void toonInformatie(Figuur figuur) {
